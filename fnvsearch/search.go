dc07634e4090e6f9232c07393c1b262114381257
Slavomir <gagliardetto@users.noreply.github.com>
2021-04-06 18:49:53 +0200

    Add fnvsearch package

---
+++ fnvsearch/search.go
@@ -0,0 +1,203 @@
+package fnvsearch
+
+import (
+	"bytes"
+	"hash"
+	"hash/fnv"
+	"io"
+	"sort"
+	"sync"
+)
+
+var hasherPool128a *sync.Pool
+
+func init() {
+	hasherPool128a = &sync.Pool{
+		New: func() interface{} {
+			return fnv.New128a()
+		},
+	}
+}
+
+// HashString returns a hash.Hash obtained from
+// a FNV-1a 128bit hash of the provided string.
+func HashString(s string) []byte {
+	return HashBytes([]byte(s))
+}
+
+// HashBytes returns a hash.Hash obtained from
+// a FNV-1a 128bit hash of the provided byte slice.
+func HashBytes(b []byte) []byte {
+	h := hasherPool128a.Get().(hash.Hash)
+
+	defer hasherPool128a.Put(h)
+	h.Reset()
+
+	if _, err := h.Write(b); err != nil {
+		panic(err)
+	}
+
+	return h.Sum(nil)
+}
+
+// HashReader returns a hash.Hash obtained from
+// a FNV-1a 128bit hash of the contents of the provided reader.
+func HashReader(reader io.Reader) []byte {
+	h := hasherPool128a.Get().(hash.Hash)
+
+	defer hasherPool128a.Put(h)
+	h.Reset()
+
+	if _, err := io.Copy(h, reader); err != nil {
+		panic(err)
+	}
+
+	return h.Sum(nil)
+}
+
+// SearchByteSlices searches for x in a sorted slice of []byte and returns the index
+// as specified by Search. The return value is the index to insert x if x is not
+// present (it could be len(a)).
+// The slice must be sorted in ascending order.
+//
+func SearchByteSlices(a [][]byte, x []byte) int {
+	return sort.Search(len(a), func(i int) bool { comp := bytes.Compare(a[i], x); return comp >= 0 })
+}
+
+// ByteSliceSlice attaches the methods of sort.Interface to [][]byte, sorting in increasing order.
+type ByteSliceSlice [][]byte
+
+func (p ByteSliceSlice) Len() int           { return len(p) }
+func (p ByteSliceSlice) Less(i, j int) bool { return bytes.Compare(p[i], p[j]) == -1 }
+func (p ByteSliceSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+// Searcher is used to track hashes.
+type Searcher struct {
+	mu     *sync.RWMutex
+	hashes [][]byte
+}
+
+// NewSearcher initializes and returns a new Searcher object.
+func NewSearcher() *Searcher {
+	return &Searcher{
+		mu: &sync.RWMutex{},
+	}
+}
+
+// WARNING_UnsortedAppendHashFromString is a non-safe method that appends (without order considerations)
+// a new hash (obtained by hashing the provided string) to the hash tracker.
+func (hs *Searcher) WARNING_UnsortedAppendHashFromString(item string) {
+	hs.WARNING_UnsortedAppendHashFromBytes([]byte(item))
+}
+
+// WARNING_UnsortedAppendHashFromBytes is a non-safe method that appends (without order considerations)
+// a new hash (obtained by hashing the provided byte slice) to the hash tracker.
+func (hs *Searcher) WARNING_UnsortedAppendHashFromBytes(item []byte) {
+	hs.WARNING_UnsortedAppendHash(HashBytes(item))
+}
+
+// WARNING_UnsortedAppendHash is a non-safe method that appends (without order considerations)
+// a new hash to the hash tracker.
+func (hs *Searcher) WARNING_UnsortedAppendHash(hash []byte) {
+	hs.mu.Lock()
+	hs.hashes = append(hs.hashes, hash)
+	hs.mu.Unlock()
+}
+
+// Sort sorts the hash tracker slice.
+func (hs *Searcher) Sort() {
+	hs.mu.Lock()
+	sort.Sort(ByteSliceSlice(hs.hashes))
+	hs.mu.Unlock()
+}
+
+// AddFromString adds a new hash obtained from hashing the provided string.
+func (hs *Searcher) AddFromString(item string) {
+	hs.AddFromBytes([]byte(item))
+}
+
+// AddFromBytes adds a new hash obtained from hashing the provided byte slice.
+func (hs *Searcher) AddFromBytes(item []byte) {
+	hs.AddHash(HashBytes(item))
+}
+
+// AddFromReader adds a new hash obtained from hashing the contents of the provided io.Reader.
+func (hs *Searcher) AddFromReader(reader io.Reader) {
+	hs.AddHash(HashReader(reader))
+}
+
+// AddHash adds the provided hash to the hash tracker.
+func (hs *Searcher) AddHash(hash []byte) {
+	hs.mu.Lock()
+	hs.noMutexAddSortedHash(hash)
+	hs.mu.Unlock()
+}
+
+// HasString returns true if the hash tracker contains the hash of the provided string.
+func (hs *Searcher) HasString(item string) bool {
+	return hs.HasBytes([]byte(item))
+}
+
+// HasBytes returns true if the hash tracker contains the hash of the provided byte slice.
+func (hs *Searcher) HasBytes(item []byte) bool {
+	return hs.HasByHash(HashBytes(item))
+}
+
+// HasFromreader returns true if the hash tracker contains the hash of the contents of the provided io.Reader.
+func (hs *Searcher) HasFromreader(reader io.Reader) bool {
+	return hs.HasByHash(HashReader(reader))
+}
+
+// HasByHash returns true if the hash tracker contains the provided hash.
+func (hs *Searcher) HasByHash(hash []byte) bool {
+	hs.mu.RLock()
+	has := hs.noMutexHas(hash)
+	hs.mu.RUnlock()
+	return has
+}
+
+// HasOrAddFromString returns true if the hash tracker contains the hash of the provided string;
+// if it does not, then the hash will be added.
+func (hs *Searcher) HasOrAddFromString(item string) bool {
+	return hs.HasOrAddHash(HashString(item))
+}
+
+// HasOrAddFromBytes returns true if the hash tracker contains the hash of the provided byte slice;
+// if it does not, then the hash will be added.
+func (hs *Searcher) HasOrAddFromBytes(item []byte) bool {
+	return hs.HasOrAddHash(HashBytes(item))
+}
+
+// HasOrAddFromReader returns true if the hash tracker contains the hash of the contents of the provided io.Reader;
+// if it does not, then the hash will be added.
+func (hs *Searcher) HasOrAddFromReader(reader io.Reader) bool {
+	return hs.HasOrAddHash(HashReader(reader))
+}
+
+// HasOrAddHash returns true if the hash tracker contains the provided hash;
+// if it does not, then the hash will be added.
+func (hs *Searcher) HasOrAddHash(hash []byte) bool {
+	hs.mu.Lock()
+	has := hs.noMutexHas(hash)
+	if !has {
+		hs.noMutexAddSortedHash(hash)
+	}
+	hs.mu.Unlock()
+	return has
+}
+
+func (hs *Searcher) noMutexAddSortedHash(hash []byte) {
+	i := SearchByteSlices(hs.hashes, hash)
+	hs.hashes = append(hs.hashes, nil /* use the zero value of the element type */)
+	copy(hs.hashes[i+1:], hs.hashes[i:])
+	hs.hashes[i] = hash
+}
+
+func (hs *Searcher) noMutexHas(hash []byte) bool {
+	index := SearchByteSlices(hs.hashes, hash)
+	if index == len(hs.hashes) {
+		return false
+	}
+	// has:
+	return bytes.Equal(hs.hashes[index], hash)
+}
